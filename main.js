65442ba0f54a833545f1e156f5f862868453ecc7
miriamela <grossi.miriam@gmail.com>
2020-07-07 18:05:40 +0200

    commented code

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//random number, take it as it is....
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max); 
 };
@@ -12,9 +12,10 @@ const numberIntroduced=document.querySelector('.input');
 const clue=document.querySelector('.clue');
 const attempt=document.querySelector('.attempt');
 
-function renderHint( message ) {
+function renderHint( message ) { //microfunction redering clue.inneHTML, slightly useless
     clue.innerHTML = message;
 }
+//main function painting clues
 
 function generateHint(event){
     event.preventDefault();
@@ -33,14 +34,14 @@ function generateHint(event){
     }
     
 };
-
+//function for counting, remember the variable needs to be a let and not a const or it won't work.
 let count=0;
 function counting(){
     count+=1;
     attempt.innerHTML = "Número de intentos: " + count;    
 }
 
-
+//the olf getTogether function
 function processingNumber(event){
     generateHint(event);
     counting();
@@ -48,66 +49,3 @@ function processingNumber(event){
 
 submit.addEventListener('click', processingNumber);
 
-
-// //correction
-
-// function getRandomNumber(max) {
-//     return Math.ceil(Math.random() * max);
-    
-// };
-
-// const randomNumber=getRandomNumber(100);
-
-// console.log(randomNumber)
-// // first 
-// const submit=document.querySelector('.submit');
-
-// function hope(){
-//     console.log(`me han clicado`)
-
-//     if(numberIntroduced.value< 1 ||numberIntroduced.value >100){ //condición más estricta, la pones al principio y la condición era esa no la tercera que pusiste tu.
-//         clue.innerHTML=`Pista: El número debe estar
-//         entre 1 y 100.`
-//     }
-//     else if (numberIntroduced.value < randomNumber){
-//         console.log(`es menor`);
-//         clue.innerHTML= `Pista: Demasiado bajo`
-//     }
-//     else if(numberIntroduced.value > randomNumber){
-//         console.log(`es menor`);
-//         clue.innerHTML= `Pista: Demasiado alto`
-//     }
-//     else if( parceInt(numberIntroduced.value) === randomNumber){ //con el triple = solo no funciona, o doble == o parceInt al numberIntroduced.value (o Number donde mantine los decimales, con parceInt te lo pasa a numero intero) porque viene del HTML como string y no como numero
-//         console.log(`no me entero de lo que pasa`)
-//         clue.innerHTML=`Has ganado campeona!!!`
-//     }
-//    // else if( numberIntroduced.value === isNaN || numberIntroduced === ''){ 
-//         // clue.value=`El número debe estar
-//         // entre 1 y 100.`
-//     //} ESO TODO ES INCORRECTO
-// }
-//  // MOSTRAR INTENTOS
-
-//  //incrementar un numero en HTML, cada vez que hacemos click
-
-//  function counting(){
-//     parceInt(attempt.innerHTML)+1
-      
-// }
-// //OR
-
-// let count=0
-// function counting(){
-//     count= count+1
-//     attempt.innerHTML= count;
-// }
-
-// //function conjunta
-// function together(ev){
-//     ev.preventDefault()
-//     hope()
-//     counting()
-
-// }
-
-// submit.addEventListener('click', together);
